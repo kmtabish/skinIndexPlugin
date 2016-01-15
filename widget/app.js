88998692ca01027406630f22ea6e963d9373c16f
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-15 14:41:47 +0530

    Added view switcher directive and view stack service for maintaining views

--- widget/app.js
+++ widget/app.js
@@ -2,24 +2,92 @@
 (function (angular) {
     angular
         .module('skinIndexPluginWidget', ['ngRoute'])
-        .config(['$routeProvider', function ($routeProvider) {
-            $routeProvider
-                .when('/', {
-                    templateUrl: 'templates/step1.html',
-                    controllerAs: 'WidgetHome',
-                    controller: 'WidgetHomeCtrl'
-                })
-                .when('/select', {
-                    templateUrl: 'templates/step2.html',
-                    controllerAs: 'WidgetList',
-                    controller: 'WidgetListCtrl'
-                })
-                .when('/location', {
-                    templateUrl: 'templates/step2.html',
-                    controllerAs: 'WidgetLocation',
-                    controller: 'WidgetLocationCtrl'
-                })
-                .otherwise('/');
+      .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
+
+        /**
+         * To make href urls safe on mobile
+         */
+        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
+
+      }])
+      .directive("viewSwitcher", ["ViewStack", "$rootScope", '$compile',
+        function (ViewStack, $rootScope, $compile) {
+          return {
+            restrict: 'AE',
+            link: function (scope, elem, attrs) {
+              var views = 0,
+                currentView = null;
+              manageDisplay();
+              $rootScope.$on('VIEW_CHANGED', function (e, type, view, noAnimation) {
+                if (type === 'PUSH') {
+                  console.log("VIEW_CHANGED>>>>>>>>");
+                  currentView = ViewStack.getPreviousView();
+
+                  var _el = $("<a/>").attr("href", "javascript:void(0)"),
+                    oldTemplate = $('#' + currentView.template);
+
+                  oldTemplate.append(_el);
+
+                  oldTemplate.find("input[type=number],input[type=text]").each(function () {
+                    $(this).blur().attr("disabled", "disabled");
+                  });
+
+                  $(document.activeElement).blur();
+                  _el.focus();
+
+                  var newScope = $rootScope.$new();
+                  var _newView = '<div  id="' + view.template + '" ><div class="slide content" ng-include="\'templates/' + view.template + '.html\'"></div></div>';
+                  var parTpl = $compile(_newView)(newScope);
+
+                  $(elem).append(parTpl);
+                  views++;
+
+                } else if (type === 'POP') {
+
+                  var _elToRemove = $(elem).find('#' + view.template),
+                    _child = _elToRemove.children("div").eq(0);
+
+                  _child.addClass("ng-leave ng-leave-active");
+                  _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
+                    _elToRemove.remove();
+                    views--;
+                  });
+
+                  currentView = ViewStack.getCurrentView();
+                  $('#' + currentView.template).find("input[type=number], input[type=text]").each(function () {
+                    $(this).removeAttr("disabled");
+                  });
+
+                } else if (type === 'POPALL') {
+                  angular.forEach(view, function (value, key) {
+                    var _elToRemove = $(elem).find('#' + value.template),
+                      _child = _elToRemove.children("div").eq(0);
+
+                    if (!noAnimation) {
+                      _child.addClass("ng-leave ng-leave-active");
+                      _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
+                        _elToRemove.remove();
+                        views--;
+                      });
+                    } else {
+                      _elToRemove.remove();
+                      views--;
+                    }
+                  });
+                }
+                manageDisplay();
+              });
+
+              function manageDisplay() {
+                if (views) {
+                  $(elem).removeClass("ng-hide");
+                } else {
+                  $(elem).addClass("ng-hide");
+                }
+              }
+
+            }
+          };
         }])
 })(window.angular);
 
--- widget/app.services.js
+++ widget/app.services.js
@@ -55,4 +55,44 @@
         }
       }
     }])
+    .factory('ViewStack', ['$rootScope', function ($rootScope) {
+      var views = [];
+      var viewMap = {};
+      return {
+        push: function (view) {
+          console.log("^^^^^^^^^^^^^^^^^");
+          if (viewMap[view.template]) {
+            this.pop();
+          } else {
+            viewMap[view.template] = 1;
+            views.push(view);
+            $rootScope.$broadcast('VIEW_CHANGED', 'PUSH', view);
+          }
+          return view;
+        },
+        pop: function () {
+          $rootScope.$broadcast('BEFORE_POP', views[views.length - 1]);
+          var view = views.pop();
+          delete viewMap[view.template];
+          $rootScope.$broadcast('VIEW_CHANGED', 'POP', view);
+
+          return view;
+        },
+        hasViews: function () {
+          return !!views.length;
+        },
+        getPreviousView: function() {
+          return views.length && views[views.length - 2] || {};
+        },
+        getCurrentView: function () {
+          return views.length && views[views.length - 1] || {};
+        },
+        popAllViews: function (noAnimation) {
+          $rootScope.$broadcast('BEFORE_POP', null);
+          $rootScope.$broadcast('VIEW_CHANGED', 'POPALL', views, noAnimation);
+          views = [];
+          viewMap = {};
+        }
+      };
+    }])
 })(window.angular, window.buildfire);
