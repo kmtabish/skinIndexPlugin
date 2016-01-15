411d4e815435d4d46fb7a6c92a3b0d944788d7be
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-15 17:24:17 +0530

    Added google places autocomplete

--- widget/app.js
+++ widget/app.js
@@ -1,93 +1,117 @@
 'use strict';
 (function (angular) {
-    angular
-        .module('skinIndexPluginWidget', ['ngRoute'])
-      .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
+  angular
+    .module('skinIndexPluginWidget', ['ngRoute'])
+    .config(['$routeProvider', '$compileProvider', function ($routeProvider, $compileProvider) {
 
-        /**
-         * To make href urls safe on mobile
-         */
-        $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
+      /**
+       * To make href urls safe on mobile
+       */
+      $compileProvider.aHrefSanitizationWhitelist(/^\s*(https?|ftp|mailto|chrome-extension|cdvfile|file):/);
 
-      }])
-      .directive("viewSwitcher", ["ViewStack", "$rootScope", '$compile',
-        function (ViewStack, $rootScope, $compile) {
-          return {
-            restrict: 'AE',
-            link: function (scope, elem, attrs) {
-              var views = 0,
-                currentView = null;
-              manageDisplay();
-              $rootScope.$on('VIEW_CHANGED', function (e, type, view, noAnimation) {
-                if (type === 'PUSH') {
-                  console.log("VIEW_CHANGED>>>>>>>>");
-                  currentView = ViewStack.getPreviousView();
+    }])
+    .directive("viewSwitcher", ["ViewStack", "$rootScope", '$compile',
+      function (ViewStack, $rootScope, $compile) {
+        return {
+          restrict: 'AE',
+          link: function (scope, elem, attrs) {
+            var views = 0,
+              currentView = null;
+            manageDisplay();
+            $rootScope.$on('VIEW_CHANGED', function (e, type, view, noAnimation) {
+              if (type === 'PUSH') {
+                console.log("VIEW_CHANGED>>>>>>>>");
+                currentView = ViewStack.getPreviousView();
 
-                  var _el = $("<a/>").attr("href", "javascript:void(0)"),
-                    oldTemplate = $('#' + currentView.template);
+                var _el = $("<a/>").attr("href", "javascript:void(0)"),
+                  oldTemplate = $('#' + currentView.template);
 
-                  oldTemplate.append(_el);
+                oldTemplate.append(_el);
 
-                  oldTemplate.find("input[type=number],input[type=text]").each(function () {
-                    $(this).blur().attr("disabled", "disabled");
-                  });
+                oldTemplate.find("input[type=number],input[type=text]").each(function () {
+                  $(this).blur().attr("disabled", "disabled");
+                });
 
-                  $(document.activeElement).blur();
-                  _el.focus();
+                $(document.activeElement).blur();
+                _el.focus();
 
-                  var newScope = $rootScope.$new();
-                  var _newView = '<div  id="' + view.template + '" ><div class="slide content" ng-include="\'templates/' + view.template + '.html\'"></div></div>';
-                  var parTpl = $compile(_newView)(newScope);
+                var newScope = $rootScope.$new();
+                var _newView = '<div  id="' + view.template + '" ><div class="slide content" ng-include="\'templates/' + view.template + '.html\'"></div></div>';
+                var parTpl = $compile(_newView)(newScope);
 
-                  $(elem).append(parTpl);
-                  views++;
+                $(elem).append(parTpl);
+                views++;
 
-                } else if (type === 'POP') {
+              } else if (type === 'POP') {
 
-                  var _elToRemove = $(elem).find('#' + view.template),
+                var _elToRemove = $(elem).find('#' + view.template),
+                  _child = _elToRemove.children("div").eq(0);
+
+                _child.addClass("ng-leave ng-leave-active");
+                _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
+                  _elToRemove.remove();
+                  views--;
+                });
+
+                currentView = ViewStack.getCurrentView();
+                $('#' + currentView.template).find("input[type=number], input[type=text]").each(function () {
+                  $(this).removeAttr("disabled");
+                });
+
+              } else if (type === 'POPALL') {
+                angular.forEach(view, function (value, key) {
+                  var _elToRemove = $(elem).find('#' + value.template),
                     _child = _elToRemove.children("div").eq(0);
 
-                  _child.addClass("ng-leave ng-leave-active");
-                  _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
-                    _elToRemove.remove();
-                    views--;
-                  });
-
-                  currentView = ViewStack.getCurrentView();
-                  $('#' + currentView.template).find("input[type=number], input[type=text]").each(function () {
-                    $(this).removeAttr("disabled");
-                  });
-
-                } else if (type === 'POPALL') {
-                  angular.forEach(view, function (value, key) {
-                    var _elToRemove = $(elem).find('#' + value.template),
-                      _child = _elToRemove.children("div").eq(0);
-
-                    if (!noAnimation) {
-                      _child.addClass("ng-leave ng-leave-active");
-                      _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
-                        _elToRemove.remove();
-                        views--;
-                      });
-                    } else {
+                  if (!noAnimation) {
+                    _child.addClass("ng-leave ng-leave-active");
+                    _child.one("webkitTransitionEnd transitionend oTransitionEnd", function (e) {
                       _elToRemove.remove();
                       views--;
-                    }
-                  });
-                }
-                manageDisplay();
-              });
-
-              function manageDisplay() {
-                if (views) {
-                  $(elem).removeClass("ng-hide");
-                } else {
-                  $(elem).addClass("ng-hide");
-                }
+                    });
+                  } else {
+                    _elToRemove.remove();
+                    views--;
+                  }
+                });
               }
+              manageDisplay();
+            });
 
+            function manageDisplay() {
+              if (views) {
+                $(elem).removeClass("ng-hide");
+              } else {
+                $(elem).addClass("ng-hide");
+              }
             }
+
+          }
+        };
+      }])
+    .directive('googleLocationSearch', function () {
+      return {
+        restrict: 'A',
+        scope: {setLocationInController: '&callbackFn'},
+        link: function (scope, element, attributes) {
+          var options = {
+            types: ['geocode']
           };
-        }])
+          var autocomplete = new google.maps.places.Autocomplete(element[0], options);
+          google.maps.event.addListener(autocomplete, 'place_changed', function () {
+            var location = autocomplete.getPlace().formatted_address;
+            if (autocomplete.getPlace().geometry) {
+              var coordinates = [autocomplete.getPlace().geometry.location.lng(), autocomplete.getPlace().geometry.location.lat()];
+              scope.setLocationInController({
+                data: {
+                  location: location,
+                  coordinates: coordinates
+                }
+              });
+            }
+          });
+        }
+      };
+    })
 })(window.angular);
 
--- widget/controllers/widget.location.controller.js
+++ widget/controllers/widget.location.controller.js
@@ -11,5 +11,17 @@
             template: 'Info'
           });
         };
+
+        WidgetLocation.setLocation = function (data) {
+          WidgetLocation.data = {
+            widget: {
+              location: data.location,
+              location_coordinates: data.coordinates
+            }
+          };
+          WidgetLocation.currentLocation = WidgetLocation.data.widget.address.location;
+          WidgetLocation.currentCoordinates = WidgetLocation.data.widget.address.location_coordinates;
+          $scope.$digest();
+        };
       }]);
 })(window.angular);
