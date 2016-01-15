a1e4d01e415c0ac805acc11d699594316f3265ab
Vishnu Singh <vish250491@gmail.com>
2016-01-15 16:02:07 +0530

    design side set bg image functionality added

--- control/design/app.js
+++ control/design/app.js
@@ -7,7 +7,26 @@
                 .when('/', {
                     templateUrl: 'templates/home.html',
                     controllerAs: 'DesignHome',
-                    controller: 'DesignHomeCtrl'
+                    controller: 'DesignHomeCtrl',
+                    resolve: {
+                        skinIndexInfo: ['DB', 'COLLECTIONS', '$q', function (DB, COLLECTIONS, $q) {
+                            var skinIndexInfo = new DB(COLLECTIONS.skinIndexInfo)
+                                , deferred = $q.defer()
+                                , success = function (result) {
+                                    if (Object.keys(result.data).length > 0) {
+                                        deferred.resolve(result);
+                                    }
+                                    else {
+                                        deferred.resolve(null);
+                                    }
+                                }
+                                , error = function (err) {
+                                    deferred.resolve(null);
+                                };
+                            skinIndexInfo.get().then(success, error);
+                            return deferred.promise;
+                        }]
+                    }
                 })
                 .otherwise('/');
         }])
--- control/design/controllers/design.home.controller.js
+++ control/design/controllers/design.home.controller.js
@@ -1,11 +1,93 @@
 'use strict';
 
-(function (angular) {
+(function (angular,buildfire) {
     angular
         .module('skinIndexPluginDesign')
-        .controller('DesignHomeCtrl', ['$scope', 'Buildfire', 'DataStore', 'TAG_NAMES',
-            function ($scope, Buildfire, DataStore, TAG_NAMES) {
-                var DesignHome = this;
-                DesignHome.background = "Background Image"
-            }]);
-})(window.angular);
+        .controller('DesignHomeCtrl', ['COLLECTIONS','DB','$scope', '$timeout', 'Buildfire','skinIndexInfo', function (COLLECTIONS,DB,$scope, $timeout, Buildfire,skinIndexInfo) {
+            var DesignHome = this,
+                _data = {
+
+                    design: {
+
+                        secListBGImage: ""
+                    }
+                };
+            var background = new Buildfire.components.images.thumbnail("#background");
+
+            if (skinIndexInfo) {
+                DesignHome.skinIndexInfo = skinIndexInfo;
+                DesignHome._lastSaved = angular.copy(DesignHome.skinIndexInfo);
+            }
+            else {
+                DesignHome.skinIndexInfo = {data: angular.copy(_data)};
+                DesignHome._lastSaved = angular.copy(DesignHome.skinIndexInfo);
+            }
+
+            if (DesignHome.skinIndexInfo.data && DesignHome.skinIndexInfo.data.design && DesignHome.skinIndexInfo.data.design.secListBGImage) {
+                background.loadbackground(DesignHome.skinIndexInfo.data.design.secListBGImage);
+            }
+
+            var tmrDelay=null;
+            DesignHome._skinIndexInfo = new DB(COLLECTIONS.skinIndexInfo);
+
+            background.onChange = function (url) {
+
+
+                DesignHome.skinIndexInfo.data.design.secListBGImage = url;
+                    console.log('DesignHomeCtrl bfURL saved :');
+                    if (!$scope.$$phase && !$scope.$root.$$phase) {
+                        $scope.$apply();
+                    }
+
+                console.log('bg change :',url);
+            };
+
+
+
+            background.onDelete = function (url) {
+                DesignHome.skinIndexInfo.data.design.secListBGImage = url;
+                if (!$scope.$$phase && !$scope.$root.$$phase) {
+                    $scope.$apply();
+                }
+                console.log('bg deleted :',url);
+            };
+
+            function isUnchanged(data) {
+                return angular.equals(data, DesignHome._lastSaved);
+            }
+
+            var updatefn = function (newObj) {
+                if (newObj) {
+                    if (tmrDelay) {
+                        clearTimeout(tmrDelay);
+                    }
+                    if (isUnchanged(newObj)) {
+                        return;
+                    }
+                    if (newObj.id) {
+                        tmrDelay = $timeout(function () {
+                            DesignHome._skinIndexInfo.update(newObj.id, newObj.data).then(function (result) {
+                                DesignHome._lastSaved = angular.copy(DesignHome.skinIndexInfo);
+                            }, function (err) {
+                                console.log(err);
+                                DesignHome.skinIndexInfo = angular.copy(DesignHome._lastSaved);
+                            });
+                        }, 500);
+                    } else {
+                        tmrDelay = $timeout(function () {
+                            DesignHome._skinIndexInfo.save(DesignHome.skinIndexInfo.data).then(function success(result) {
+                            }, function fail(err) {
+                                console.log(err);
+                            });
+                        }, 500);
+                    }
+                }
+            };
+
+
+            $scope.$watch(function () {
+                return DesignHome.skinIndexInfo;
+            }, updatefn, true);
+
+        }]);
+})(window.angular,buildfire);
