bb30c13bc2c8713eac6a13ba67c231614abcabc6
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-18 16:37:47 +0530

    Showed recommendation according to UV index of given location selected

--- widget/enums.js
+++ widget/enums.js
@@ -26,4 +26,63 @@
     .constant('WEATHER_UNDERGROUND', {
       API_KEY: '9a2afe49784d882f'
     })
+    .constant('RECOMMENDATIONS', {
+      0: {
+        condition: 'Low',
+        title: 'You can safely stay outside'
+      },
+      1: {
+        condition: 'Low',
+        title: 'You can safely stay outside'
+      },
+      2: {
+        condition: 'Low',
+        title: 'You can safely stay outside'
+      },
+      3: {
+        condition: 'Moderate',
+        title: 'Take precautions',
+        steps: ['UVO', 'Suncreen 30+']
+      },
+      4: {
+        condition: 'Moderate',
+        title: 'Take precautions',
+        steps: ['UVO', 'Suncreen 30+']
+      },
+      5: {
+        condition: 'Moderate',
+        title: 'Take precautions',
+        steps: ['UVO', 'Suncreen 30+']
+      },
+      6: {
+        condition: 'High',
+        title: 'Protection Needed',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses']
+      },
+      7: {
+        condition: 'High',
+        title: 'Protection Needed',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses']
+      },
+      8: {
+        condition: 'Very High',
+        title: 'Caution Advised',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses', 'Seek Shade']
+      },
+      9: {
+        condition: 'Very High',
+        title: 'Caution Advised',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses', 'Seek Shade']
+      },
+      10: {
+        condition: 'Very High',
+        title: 'Caution Advised',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses', 'Seek Shade']
+      },
+      11: {
+        condition: 'Extreme',
+        title: 'Avoid being outdoors',
+        steps: ['UVO', 'Sunscreen 30+', 'Hat/Sunglasses', 'Seek Shade']
+      }
+    })
 })(window.angular);
