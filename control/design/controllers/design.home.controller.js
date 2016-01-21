a10b1541d34f5b21e04df03cf0058d0f794f5081
Vishnu Singh <vish250491@gmail.com>
2016-01-21 15:34:40 +0530

    design side code refactored due to redundancy issue in setting data in datastor

--- control/design/controllers/design.home.controller.js
+++ control/design/controllers/design.home.controller.js
@@ -3,21 +3,31 @@
 (function (angular, window) {
   angular
     .module('skinIndexPluginDesign')
-    .controller('DesignHomeCtrl', ['TAG_NAMES', 'DataStore', '$scope', 'Buildfire', function (TAG_NAMES, DataStore, $scope, Buildfire) {
+    .controller('DesignHomeCtrl', ['TAG_NAMES', 'DataStore', '$scope', 'Buildfire',
+        function (TAG_NAMES, DataStore, $scope, Buildfire) {
 
-      var DesignHome = this;
-      DesignHome.masterData = null;
+          var DesignHome = this;
+          DesignHome.masterData = null;
+
+        function updateMasterItem(data) {
+          console.log('Design home updateMasterItem---->',data);
+          DesignHome.masterData = angular.copy(data);
+        }
+
+        function isUnchanged(data) {
+          console.log('Design home isUnchanged---->',data);
+          return angular.equals(data, DesignHome.masterData);
+        }
 
 
-      DesignHome.saveData = function (newObj, tag) {
+        DesignHome.saveData = function (newObj, tag) {
           console.log('DesignHome.saveData ---------->',newObj);
         if (typeof newObj === 'undefined') {
           return;
         }
         DesignHome.success = function (result) {
             console.log('DesignHome.saveData success---------->',result);
-          console.info('Saved data result: ', result);
-          updateMasterItem(newObj);
+            updateMasterItem(newObj);
         };
         DesignHome.error = function (err) {
           console.error('Error while saving data : ', err);
@@ -25,18 +35,35 @@
         DataStore.save(newObj, tag).then(DesignHome.success, DesignHome.error);
       };
 
+
+        /*Init method call, it will bring all the pre saved data*/
+        DesignHome.init = function () {
+          DesignHome.success = function (result) {
+            console.info('init success result:', result);
+            if (result) {
+              DesignHome.data = result.data;
+              if (!DesignHome.data.design)
+                DesignHome.data.design = {};
+            }
+            if (DesignHome.data.design.secListBGImage)
+              background.loadbackground(DesignHome.data.design.secListBGImage);
+
+          };
+          DesignHome.error = function (err) {
+            console.error('##############Error while getting data', err);
+          };
+          DataStore.get(TAG_NAMES.UVO_INFO).then(DesignHome.success, DesignHome.error);
+        };
+
+        DesignHome.init();
+
       var background = new Buildfire.components.images.thumbnail("#background");
 
       background.onChange = function (url) {
-
         DesignHome.data.design.secListBGImage = url;
-        DesignHome.success = function (response) {
-          console.log(response)
-        };
-        DesignHome.error = function (error) {
-          console.log(error)
-        };
-        DataStore.save(DesignHome, TAG_NAMES.UVO_INFO).then(DesignHome.success, DesignHome.error);
+        if (!$scope.$$phase && !$scope.$root.$$phase) {
+          $scope.$apply();
+        }
         console.log('bg change :', url);
       };
 
@@ -47,39 +74,6 @@
           $scope.$apply();
         }
       };
-
-      function updateMasterItem(data) {
-          console.log('Design home updateMasterItem---->',data);
-        DesignHome.masterData = angular.copy(data);
-      }
-
-
-      /*Init method call, it will bring all the pre saved data*/
-      DesignHome.init = function () {
-        DesignHome.success = function (result) {
-          console.info('init success result:', result);
-          if (result) {
-            DesignHome.data = result.data;
-              if (!DesignHome.data.design)
-                  DesignHome.data.design = {};
-                }
-            if (DesignHome.data.design.secListBGImage)
-            background.loadbackground(DesignHome.data.design.secListBGImage);
-
-        };
-        DesignHome.error = function (err) {
-          console.error('Error while getting data', err);
-        };
-        DataStore.get(TAG_NAMES.UVO_INFO).then(DesignHome.success, DesignHome.error);
-      };
-
-      DesignHome.init();
-
-
-      function isUnchanged(data) {
-          console.log('Design home isUnchanged---->',data);
-        return angular.equals(data, DesignHome._lastSaved);
-      }
 
       var tmrDelay = null;
       var saveDataWithDelay = function (newObj) {
