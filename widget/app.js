39b09e1e9772ff881138f7c0f8cafc385b590644
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-28 12:03:07 +0530

    disabled fast click for google autocomplete

--- widget/app.js
+++ widget/app.js
@@ -37,16 +37,16 @@
 
                 var newScope = $rootScope.$new();
                 var _newView;
-                if(view.template=="Location"){
+                if (view.template == "Location") {
                   _newView = '<div  id="' + view.template + '" ><div class="slide content ng-enter ng-enter-active"  ng-include="\'templates/' + view.template + '.html\'"></div></div>';
-                }else{
+                } else {
                   _newView = '<div  id="' + view.template + '" ><div class="slide infopage content" data-back-img="{{itemDetailsBackgroundImage}}" ng-include="\'templates/' + view.template + '.html\'"></div></div>';
                 }
                 var parTpl = $compile(_newView)(newScope);
 
                 $(elem).append(parTpl);
 
-                setTimeout(function() {
+                setTimeout(function () {
                   parTpl.children("div").eq(0).removeClass("ng-enter");
                   newScope.$apply();
                 }, 200);
@@ -141,10 +141,17 @@
             types: ['(cities)']
           };
           var autocomplete = new google.maps.places.Autocomplete(element[0], options);
+
+          $(document).on({
+            'DOMNodeInserted': function() {
+              $('.pac-item, .pac-item span', this).addClass('needsclick');
+            }
+          }, '.pac-container');
+
           google.maps.event.addListener(autocomplete, 'place_changed', function () {
             var location = autocomplete.getPlace().formatted_address;
             if (autocomplete.getPlace().geometry) {
-              var coordinates = [autocomplete.getPlace().geometry.location.lat(),autocomplete.getPlace().geometry.location.lng()];
+              var coordinates = [autocomplete.getPlace().geometry.location.lat(), autocomplete.getPlace().geometry.location.lng()];
               scope.setLocationInController({
                 data: {
                   location: location,
