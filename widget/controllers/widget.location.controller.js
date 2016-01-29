2a98d3250b5efab56e6e88fe3fdc7a92c389b3be
Sakshi Tyagi <sakshi.tyagi@tothenew.com>
2016-01-29 20:03:24 +0530

    FIxed last letter remaining in  location box even after deleting

--- widget/controllers/widget.location.controller.js
+++ widget/controllers/widget.location.controller.js
@@ -33,11 +33,26 @@
         WidgetLocation.init();
 
         WidgetLocation.getWeatherData = function () {
-          if (JSON.parse(localStorage.getItem('LocationObject'))) {
-            ViewStack.push({
-              template: 'Weather'
-            });
-          }
+          var geocoder = new google.maps.Geocoder();
+          geocoder.geocode({"address": WidgetLocation.currentLocation}, function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+              var lat = results[0].geometry.location.lat(),
+                lng = results[0].geometry.location.lng();
+              WidgetLocation.setLocation({
+                location: WidgetLocation.currentLocation,
+                coordinates: [lng, lat]
+              });
+              if (JSON.parse(localStorage.getItem('LocationObject'))) {
+                ViewStack.push({
+                  template: 'Weather'
+                });
+              }
+            }
+            else {
+              localStorage.setItem('LocationObject', null);
+              console.log("Invalid location");
+            }
+          });
         };
 
         WidgetLocation.setLocation = function (data, redirectToNext) {
@@ -123,42 +138,6 @@
         WidgetLocation.listeners['BEFORE_POP'] = $rootScope.$on('BEFORE_POP', function (e) {
           Modals.dismiss();
         });
-
-        /*
-         * create an artificial delay so api isnt called on every character entered
-         * */
-        var tmrDelay = null;
-        var saveLocationWithDelay = function (newValue, oldValue) {
-          if (newValue) {
-            if (newValue == oldValue) {
-              return;
-            }
-            if (tmrDelay) {
-              clearTimeout(tmrDelay);
-            }
-            tmrDelay = setTimeout(function () {
-              var geocoder = new google.maps.Geocoder();
-              geocoder.geocode({"address": newValue}, function (results, status) {
-                if (status == google.maps.GeocoderStatus.OK) {
-                  var lat = results[0].geometry.location.lat(),
-                    lng = results[0].geometry.location.lng();
-                  WidgetLocation.setLocation({
-                    location: newValue,
-                    coordinates: [lng, lat]
-                  });
-                }
-                else {
-                  localStorage.setItem('LocationObject', null);
-                  console.log("Invalid location");
-                }
-              });
-            }, 500);
-          }
-        };
-
-        $scope.$watch(function () {
-          return WidgetLocation.currentLocation;
-        }, saveLocationWithDelay, true);
 
       }]);
 })(window.angular);
